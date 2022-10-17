bf33b50bc929433d94621a6894aadaf842ba6342
Elcho <98739225+Elkhan2003@users.noreply.github.com>
2022-10-17 21:44:44 +0600

    Add files via upload

---
+++ Elcho/js/script.js
@@ -0,0 +1,64 @@
+const elcho = document.querySelector('#elcho')
+elcho.innerHTML = `
+<!-- ! Начало -->
+		<div class="wrapper">
+			<div class="wrap">
+				<!-- ! header -->
+				<header class="header">
+					<div id="header__scroll"">
+						<div class="container">
+							<div class="header__block">
+								<div class="header__logo">
+									<a href="https://github.com/Elkhan2003" target="_blank"><p>GitHub</p></a>
+								</div>
+								<nav class="header__nav">
+									<ul class="header__list">
+										<li><a href="https://www.instagram.com/elcho911/" target="_blank">Instagram</a></li>
+										<li><a href="https://discord.gg/NZpxaY4CGC" target="_blank">Discord</a></li>
+										<li><a href="https://wa.me/996990385056/" target="_blank">What's App</a></li>
+										<li><a href="https://t.me/Elcho911/" target="_blank">Telegram</a></li>
+									</ul>
+								</nav>
+							</div>
+						</div>
+					</div>
+				</header>
+
+				<!-- ! main -->
+				<main class="main">
+					<!-- ! section -->
+					<section class="block__one">
+						<div class="container">
+							<div class="block__one__content">
+								<div class="block__one__buttons">
+									<button class="block__one__button button__reset">Side</button>
+									<button class="block__one__button button__reset">Side</button>
+									<button class="block__one__button button__reset">Side</button>
+									<button class="block__one__button button__reset">Side</button>
+									<button class="block__one__button button__reset">Side</button>
+									<button class="block__one__button button__reset">Side</button>
+									<button class="block__one__button button__reset">Side</button>
+									<button class="block__one__button button__reset">Side</button>
+									<button class="block__one__button button__reset">Side</button>
+									<button class="block__one__button button__reset">Side</button>
+									<button class="block__one__button button__reset">Side</button>
+									<button class="block__one__button button__reset">Side</button>
+								</div>
+
+								<!-- ! grid -->
+								<div class="block__one__grid">
+									<div class="block__one__grid__box">Big Box</div>
+									<div class="block__one__grid__box">Big Box</div>
+									<div class="block__one__grid__box">Big Box</div>
+									<div class="block__one__grid__box">Big Box</div>
+								</div>
+							</div>
+						</div>
+					</section>
+				</main>
+				
+				<!-- ! footer -->
+				<footer class="footer"></footer>
+			</div>
+		</div>
+`
